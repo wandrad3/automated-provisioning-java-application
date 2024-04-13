50bba24860dc90d36f40adcdd9fe529ef14ceafe
wandrad3 <wesleyandrade298@gmail.com>
2024-04-13 12:00:28 -0300

    automated-provisioning-java-app

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"6f7fe3e69741eda871a0795a9cafd70cc458db3041e2509b9a9a512438fb66c8","vagrant_version":"2.3.4"}
